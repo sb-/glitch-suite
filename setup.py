4396d2441a983d5d73fb07af7c7ae993aded4a97
Tom L <tomrlu@gmail.com>
2017-09-20 13:34:37 -0500

    initial with basic rgb shift

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+
+setup(
+  name = 'rgbshift',
+  packages = ['rgbshift'], # this must be the same as the name above
+  version = '0.1',
+  description = 'RGB shift CLI tool',
+  author = 'cqsd',
+  author_email = 'colloquiasd@gmail.com',
+  download_url = 'https://github.com/cqsd/glitch-suite/archive/0.1.tar.gz',
+  keywords = ['glitch'], # arbitrary keywords
+  classifiers = [],
+)
