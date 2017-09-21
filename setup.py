e550a918c07f0ee48e532c7dbac1e8b25a640bc8
Tom L <tomrlu@gmail.com>
2017-09-20 23:48:42 -0500

    add missing url in setup

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setup(
   description = 'RGB shift CLI tool',
   author = 'cqsd',
   author_email = 'colloquiasd@gmail.com',
+  url = 'https://github.com/cqsd/glitch-suite',
   download_url = 'https://github.com/cqsd/glitch-suite/archive/0.1.tar.gz',
   keywords = ['glitch'], # arbitrary keywords
   classifiers = [],
