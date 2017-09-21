91ef50172a99fc0031215e4298792c13c87f0895
Tom L <tomrlu@gmail.com>
2017-09-21 00:02:38 -0500

    version number

--- setup.py
+++ setup.py
@@ -4,11 +4,13 @@ from distutils.core import setup
 setup(
   name = 'rgbshift',
   packages = ['rgbshift'], # this must be the same as the name above
-  version = '0.1',
+  version = '0.0.1',
   description = 'RGB shift CLI tool',
   author = 'cqsd',
   author_email = 'colloquiasd@gmail.com',
-  download_url = 'https://github.com/cqsd/glitch-suite/archive/0.1.tar.gz',
+  url = 'https://github.com/cqsd/glitch-suite',
+  download_url = 'https://github.com/cqsd/glitch-suite/archive/0.0.1.tar.gz',
   keywords = ['glitch'], # arbitrary keywords
   classifiers = [],
+  scripts=['rgbshift/rgbshift']
 )
